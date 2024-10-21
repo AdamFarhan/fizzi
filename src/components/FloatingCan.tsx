ad7b86163eee285039f2f2439712b52d0e9920bf
Adam Farhan <adamfarhan11@gmail.com>
2024-10-21 13:59:47 -0400

    initial commit

---
+++ src/components/FloatingCan.tsx
@@ -0,0 +1,53 @@
+"use client";
+import { forwardRef, ReactNode } from "react";
+import { Float } from "@react-three/drei";
+
+import { SodaCan, SodaCanProps } from "@/components/SodaCan";
+import { Group } from "three";
+
+type FloatingCanProps = {
+  /** Flavor */
+  flavor?: SodaCanProps["flavor"];
+  /** Animation speed, defaults to 1.5 */
+  floatSpeed?: number;
+  /** XYZ rotation intensity, defaults to 1 */
+  rotationIntensity?: number;
+  /** Up/down float intensity, works like a multiplier with floatingRange,defaults to 1 */
+  floatIntensity?: number;
+  /** Range of y-axis values the object will float within, defaults to [-0.1,0.1] */
+  floatingRange?: [number, number];
+  children?: ReactNode;
+};
+
+const FloatingCan = forwardRef<Group, FloatingCanProps>(
+  (
+    {
+      flavor = "blackCherry",
+      floatSpeed = 1.5,
+      rotationIntensity = 1,
+      floatIntensity = 1,
+      floatingRange = [-0.1, 0.1],
+      children,
+      ...props
+    },
+    ref,
+  ) => {
+    return (
+      <group ref={ref} {...props}>
+        <Float
+          speed={floatSpeed}
+          rotationIntensity={rotationIntensity}
+          floatIntensity={floatIntensity}
+          floatingRange={floatingRange}
+        >
+          {children}
+          <SodaCan flavor={flavor} />
+        </Float>
+      </group>
+    );
+  },
+);
+
+FloatingCan.displayName = "FloatingCan";
+
+export default FloatingCan;
---
+++ src/slices/SkyDive/index.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { Container } from "@/components/Container";
+import { Content } from "@prismicio/client";
+import { SliceComponentProps } from "@prismicio/react";
+import { Scene } from "./Scene";
+import { View } from "@react-three/drei";
+
+/**
+ * Props for `SkyDive`.
+ */
+export type SkyDiveProps = SliceComponentProps<Content.SkyDiveSlice>;
+
+/**
+ * Component for "SkyDive" Slices.
+ */
+const SkyDive = ({ slice }: SkyDiveProps): JSX.Element => {
+  return (
+    <Container
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+      className="skydive h-screen"
+    >
+      <h2 className="sr-only">{slice.primary.sentence}</h2>
+      <View className="h-screen w-screen">
+        <Scene
+          flavor={slice.primary.flavor}
+          sentence={slice.primary.sentence}
+        />
+      </View>
+    </Container>
+  );
+};
+
+export default SkyDive;
